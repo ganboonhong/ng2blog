b3351f9f5e6dabbe46c99e0f695a7c00e1482f8a
ganboonhong <ganboonhong@gmail.com>
2015-11-02 01:59:54 +0800

    second commit from mac

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,28 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+/*elixir(function(mix) {
     mix.sass('app.scss');
-});
+
+    //  Note: for merging CSS files use
+    //  Third parameter is used to specify a output directory
+    /!*mix.styles([
+        'vendor/normalize.css',
+        'app.css'
+    ],'public/output/final.css', 'public/css')*!/
+
+    mix.styles([
+        'vendor/normalize.css',
+        'app.css'
+    ], null, 'public/css')
+
+    mix.version('public/css/all.css');
+});*/
+
+
+elixir(function(mix) {
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+    mix.sass('app.scss')
+        .copy(bootstrapPath + '/fonts', 'public/fonts')
+        .copy(bootstrapPath + '/javascripts/bootstrap.min.js',
+        'public/js'); });
